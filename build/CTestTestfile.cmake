90420d723de366d7efc14b87c8b37e1cb53a2ae9
fano2458 <fano2458@gmail.com>
2023-09-09 15:55:09 +0600

    initial

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/fano/Desktop/motorsports/motorsports_team_perception/src
+# Build directory: /home/fano/Desktop/motorsports/motorsports_team_perception/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/fano/Desktop/motorsports/motorsports_team_perception/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/fano/Desktop/motorsports/motorsports_team_perception/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/fano/Desktop/motorsports/motorsports_team_perception/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/fano/Desktop/motorsports/motorsports_team_perception/src:$ROS_PACKAGE_PATH"
